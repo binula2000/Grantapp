cbc603153bbd18e406e8a125fd65b3ca44ea3e0b
binula2000 <binula2000@gmail.com>
2025-10-02 21:34:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     // 👇 ADD THIS PROPERTY 👇
-    base: '/grantapp', // <-- Be sure to change this!
+    base: '/Grantapp', // <-- Be sure to change this!
 
     server: {
       port: 3000,
